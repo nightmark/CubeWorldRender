5a7c62cdcb4ed1c9efc8ee5d1b08882fc64cbb3f
Nightmark <nightmark@gmail.com>
2012-05-17 10:32:35 +0200

    fixes to the visualisation

--- src/mygame/Main.java
+++ src/mygame/Main.java
@@ -1,59 +1,87 @@
 package mygame;
 
 import com.jme3.app.SimpleApplication;
+import com.jme3.input.KeyInput;
+import com.jme3.input.controls.ActionListener;
+import com.jme3.input.controls.KeyTrigger;
 import com.jme3.material.Material;
+import com.jme3.material.RenderState.BlendMode;
 import com.jme3.math.ColorRGBA;
 import com.jme3.math.Vector3f;
 import com.jme3.renderer.RenderManager;
+import com.jme3.renderer.queue.RenderQueue.Bucket;
 import com.jme3.scene.Geometry;
 import com.jme3.scene.shape.Box;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public class Main extends SimpleApplication {
 
-    int sizeX = 0;
-    int sizeY = 0;
-    int sizeZ = 0;
-    int oldSizeX = 0;
-    int oldSizeY = 0;
-    int oldSizeZ = 0;
-    ColorRGBA world[][][];
-    Geometry worldGeometries[][][];
-    Server server;
+    private int sizeX = 0;
+    private int sizeY = 0;
+    private int sizeZ = 0;
+    private int oldSizeX = 0;
+    private int oldSizeY = 0;
+    private int oldSizeZ = 0;
+    private final int  DEFAULT_WORLD_SIZE = 32;
+    private ColorRGBA world[][][];
+    private Geometry worldGeometries[][][];
+    private Server server;
+    private AtomicBoolean updatePending = new AtomicBoolean(false);
+    private int[][][] pendingWorld = null;
     
     public static void main(String[] args) {        
-        Main app = new Main();                
-        app.start();
+        Main app = new Main();
+        app.start();        
     }
+    private double cubeId = 0d;
 
     @Override
     public void simpleInitApp() {        
         //open TCP communication        
-        setSize(32);
+        setSize(DEFAULT_WORLD_SIZE);
+        java.util.logging.Logger.getLogger("").setLevel(Level.WARNING);
         world = new ColorRGBA[sizeX][sizeY][sizeZ];
         worldGeometries = new Geometry[sizeX][sizeY][sizeZ];
         cam.setFrustumFar(30);
         flyCam.setMoveSpeed(5);
+        initKeys();
         initWorld();
         initCube();
-        initServer();
+        initServer();        
     }
+    
+    private void initKeys() {
+        inputManager.deleteMapping(SimpleApplication.INPUT_MAPPING_EXIT);
+        inputManager.addMapping("Quit",  new KeyTrigger(KeyInput.KEY_ESCAPE));
+
+        inputManager.addListener(actionListener, new String[]{"Quit"});
+ 
+    }
+    private ActionListener actionListener = new ActionListener() {
+        public void onAction(String name, boolean keyPressed, float tpf) {
+          if (name.equals("Quit") && !keyPressed) {
+              stopServer();
+              stop();
+          }
+        }
+    };
     
     public void initServer(){        
         server = new Server(this);
         new Thread(server).start();
     }
+    
+    public void stopServer(){        
+        server.stop();
+    }
 
     @Override
     public void simpleUpdate(float tpf) {
-//        if(oldSizeX != sizeX || oldSizeY != sizeY || oldSizeZ != sizeZ){
-//            for(int x = oldSizeX; x < sizeX; x++){
-//                for(int y = oldSizeY; y < sizeY; y++){
-//                    for(int z = oldSizeZ; z < sizeZ; z++){
-//                        worldGeometries[x][y][z].getMaterial().setColor("Color", new ColorRGBA(1,1,1,0));
-//                    }
-//                }
-//            }
-//        }
+        if(updatePending.get()){
+            updateCube(pendingWorld);
+        }
     }
 
     @Override
@@ -74,52 +102,66 @@ public class Main extends SimpleApplication {
         return sizeX;
     };
     
+    public synchronized void requestUpdate(int[][][] world, double id){        
+        if(updatePending.get() && cubeId != id){
+            System.out.println("Not Updating " + id);
+            return;
+        }        
+        pendingWorld = world;
+        updatePending.set(true);
+    }
+    
     public void updateCube(int[][][] world){
         System.out.println("updating cube");
-        rootNode.detachAllChildren();
-        if(oldSizeX > sizeX || oldSizeY > sizeY || oldSizeZ > sizeZ){
-            for(int x = oldSizeX; x < sizeX; x++){
-                for(int y = oldSizeY; y < sizeY; y++){
-                    for(int z = oldSizeZ; z < sizeZ; z++){
-                        worldGeometries[x][y][z] = null;
-                    }
-                }
-            }
-        }else{
-            if(oldSizeX < sizeX || oldSizeY < sizeY || oldSizeZ < sizeZ){
+        synchronized(this){
+            rootNode.detachAllChildren();
+            if(oldSizeX > sizeX || oldSizeY > sizeY || oldSizeZ > sizeZ){
                 for(int x = oldSizeX; x < sizeX; x++){
                     for(int y = oldSizeY; y < sizeY; y++){
                         for(int z = oldSizeZ; z < sizeZ; z++){
-                            Box b = new Box(new Vector3f(x, y, z), (float)0.2, (float)0.2, (float)0.2);
-                            Geometry geom = new Geometry("Box "+x+"|"+y+"|"+z, b);
-                            worldGeometries[x][y][z] = geom;
-                            Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-                            geom.setMaterial(mat);
-                            rootNode.attachChild(geom);
                             worldGeometries[x][y][z] = null;
                         }
                     }
                 }
+            }else{
+                if(oldSizeX < sizeX || oldSizeY < sizeY || oldSizeZ < sizeZ){
+                    for(int x = oldSizeX; x < sizeX; x++){
+                        for(int y = oldSizeY; y < sizeY; y++){
+                            for(int z = oldSizeZ; z < sizeZ; z++){
+                                Box b = new Box(new Vector3f(x, y, z), (float)0.2, (float)0.2, (float)0.2);
+                                Geometry geom = new Geometry("Box "+x+"|"+y+"|"+z, b);
+                                worldGeometries[x][y][z] = geom;
+                                Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
+                                mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
+                                geom.setQueueBucket(Bucket.Transparent);
+                                geom.setMaterial(mat);
+                                rootNode.attachChild(geom);                                                                
+                            }
+                        }
+                    }
+                }
             }
-        }
-        for(int x = 0; x < sizeX; x++){
-            for(int y = 0; y < sizeY; y++){
-                for(int z = 0; z < sizeZ; z++){
-                    //ColorRGBA color = new ColorRGBA(x/(float)sizeX, y/(float)sizeY, z/(float)sizeZ, 0);
-                    ColorRGBA color = new ColorRGBA(1, 1, 1, 0);
-                    worldGeometries[x][y][z].getMaterial().setColor("Color", color);
-                    rootNode.attachChild(worldGeometries[x][y][z]);
+            for(int x = 0; x < sizeX; x++){
+                for(int y = 0; y < sizeY; y++){
+                    for(int z = 0; z < sizeZ; z++){
+                        //ColorRGBA color = new ColorRGBA(x/(float)sizeX, y/(float)sizeY, z/(float)sizeZ, 0);
+                        ColorRGBA color = new ColorRGBA(world[x][y][z]/10f + 0.2f, world[x][y][z]/10f + 0.2f, world[x][y][z]/10f + 0.2f, 0.6f);
+                        //ColorRGBA color = new ColorRGBA(1, 0, 0, 0);                                                
+                        worldGeometries[x][y][z].getMaterial().setColor("Color", color);                        
+                        rootNode.attachChild(worldGeometries[x][y][z]);
+                    }
                 }
             }
         }
         System.out.println("cube updated");
+        updatePending.set(false);
     }
     
     public void initWorld(){
         for(int x = 0; x < sizeX; x++){
                 for(int y = 0; y < sizeY; y++){
                     for(int z = 0; z < sizeZ; z++){
-                        world[x][y][z] = new ColorRGBA(x/100f+0.15f, y/100f+0.3f, z/100f+0.35f, 0);
+                        world[x][y][z] = new ColorRGBA(x/100f+0.15f, y/100f+0.3f, z/100f+0.35f, 0.8f);                        
                     }
                 }
         }
@@ -133,11 +175,17 @@ public class Main extends SimpleApplication {
                     Geometry geom = new Geometry("Box "+x+"|"+y+"|"+z, b);
                     worldGeometries[x][y][z] = geom;
                     Material mat = new Material(assetManager, "Common/MatDefs/Misc/Unshaded.j3md");
-                    mat.setColor("Color", world[x][y][z]);
+                    mat.setColor("Color", world[x][y][z]);  
+                    mat.getAdditionalRenderState().setBlendMode(BlendMode.Alpha);
                     geom.setMaterial(mat);
+                    geom.setQueueBucket(Bucket.Transparent);
                     rootNode.attachChild(geom);
                 }
             }
         }
+    }
+
+    void setId(double cubeId) {
+        this.cubeId = cubeId;
     }
 }
--- src/mygame/Server.java
+++ src/mygame/Server.java
@@ -4,10 +4,15 @@
  */
 package mygame;
 
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.PrintStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -15,13 +20,14 @@ import java.nio.ByteBuffer;
  */
 public class Server implements Runnable{
     
-    Main main;
-    boolean running = false;
-    ServerSocket srvr;
+    private Main main;
+    private boolean running = false;
+    private ServerSocket srvr;
     
     private final byte INFO = 0; 
-    private final byte CHANGED_DATA = 7;
-    private final int SOCKET = 1234;
+    private final byte MANAGE_CUBE = 1;
+    private final byte CHANGED_DATA = 7;    
+    private final int PORT = 1234;
     
     public Server(Main main) {
         this.main = main;
@@ -29,7 +35,7 @@ public class Server implements Runnable{
     
     public void run() {
         try{
-            srvr = new ServerSocket(SOCKET);            
+            srvr = new ServerSocket(PORT);            
             running = true;
             System.out.println("Server Started");
             listen();
@@ -40,81 +46,136 @@ public class Server implements Runnable{
     }
     
     public void listen(){
-        while(running){
-            try{
-                System.out.println("Listening on " + SOCKET + "...");
-                Socket skt = srvr.accept();                                
-                proccessPacket(skt.getInputStream());
+        Socket skt = null;
+        try{
+            skt = srvr.accept(); 
+            while(running){
+                    //System.out.println("Listening on " + PORT + "...");
+                    proccessPacket(skt.getInputStream());
+            }
+        }catch(IOException ex){
+                    System.err.println("Server Listening Error " + ex);
+        }finally{
+            try {
                 skt.close();
-            }catch(Exception e){
-                System.err.println("Server Listening Error " + e);
+            } catch (IOException ex) {
+                System.err.println("Error closing socket " + ex);
             }
         }
+        System.out.println("Everything falls apart");
     }
+    
+    static final String HEXES = "0123456789ABCDEF";
+    public static String getHex( byte [] raw ) {
+        if ( raw == null ) {
+          return null;
+        }
+        final StringBuilder hex = new StringBuilder( 2 * raw.length );
+        for ( final byte b : raw ) {
+          hex.append(HEXES.charAt((b & 0xF0) >> 4))
+             .append(HEXES.charAt((b & 0x0F)));
+        }
+        return hex.toString();
+    }    
     
     public void proccessPacket(InputStream in){
         try{
-            int id = in.read();            
+//            byte[] buffer = new byte[4];
+//            in.read();
+//            ByteBuffer bb;
+//            int line = 0;
+//            while ((in.read(buffer)) != -1){                
+//                bb = ByteBuffer.wrap(buffer);
+//                bb.order(ByteOrder.LITTLE_ENDIAN);
+//                System.out.println(line+" :"+getHex(buffer)+" = "+bb.getInt());
+//                line++;
+//            }
+//            System.out.println();
+//            return;
+            int id = in.read();
+            System.out.println("Remaining1 " + in.available());            
             ByteBuffer bb;
             byte[] buffer;
+            double cubeId;
             if(id != -1){
                 switch(id){
                     case INFO:
                         buffer = new byte[4];
                         in.read(buffer);                        
                         bb = ByteBuffer.wrap(buffer);
+                        bb.order(ByteOrder.LITTLE_ENDIAN);
                         int cubeSize = bb.getInt();
                         System.out.println("Received Packet INFO with size " + cubeSize);
                         main.setSize(cubeSize);
                         System.out.println("setSize called");
                         bb.clear();
                         break;
+                    case MANAGE_CUBE:
+                        buffer = new byte[8];
+                        in.read(buffer);                        
+                        bb = ByteBuffer.wrap(buffer);
+                        bb.order(ByteOrder.LITTLE_ENDIAN);
+                        cubeId = bb.getDouble();
+                        in.read();
+                        System.out.println("Received Packet MANAGE_CUBE with id " + cubeId);
+                        main.setId(cubeId);                        
+                        bb.clear();
+                        System.out.println("Remaining " + in.available());
+                        break;
                     case CHANGED_DATA:
                         System.out.println("Received Packet CHANGED_DATA");
                         buffer = new byte[8];
+                        System.out.println("Remaining2 " + in.available());
                         //read ID
                         in.read(buffer);
+                        bb = ByteBuffer.wrap(buffer, 0, 8);
+                        bb.order(ByteOrder.LITTLE_ENDIAN);
+                        cubeId = bb.getDouble();
                         //read generation
                         in.read(buffer);
                         //read contents
                         //4bytes per cube cell
                         int size = main.getSize();
                         int[][][] world = new int[size][size][size];
-                        for(int x = 0; x < size; x++){
+                        System.out.println("Live Cells At:");
+                        for(int z = 0; z < size; z++){
                             for(int y = 0; y < size; y++){
-                                for(int z = 0; z < size; z++){
+                                for(int x = 0; x < size; x++){
                                     in.read(buffer, 0, 4);
                                     bb = ByteBuffer.wrap(buffer, 0, 4);
+                                    bb.order(ByteOrder.LITTLE_ENDIAN);
                                     world[x][y][z] = bb.getInt();
-                                    System.out.println("received coords " + x + " " + y + " " + z );
+                                    if(world[x][y][z] != 0){
+                                        System.out.println(x + " " + y + " " + z + " is " + world[x][y][z]);
+                                        world[x][y][z] = 8;
+                                    }                                    
+                                    //System.out.println("received coords " + x + " " + y + " " + z );
                                 }
                             }
                         }
-                        System.out.println("sending world : ");
-                        for(int x = 0; x < size; x++){
-                            for(int y = 0; y < size; y++){
-                                for(int z = 0; z < size; z++){
-                                    System.out.println(x + " " + y + " " + " " + z + " is " + world[x][y][z]);
-                                }
-                            }
+                        System.out.println("Remaining " + in.available());
+                        if(in.available() > 0){
+                            System.out.println(in.read());
                         }
                         System.out.println("world sent");
-                        main.updateCube(world);
+                        main.requestUpdate(world, cubeId);
                         break;
                     default:
                         //unknown packet
                         System.out.println("Unknown packet id:" + id);                        
                         break;
                         
-                }
+                }                
             }
         }catch(Exception e){
-            System.err.println("Error processing packet" + e);
+            System.err.println("Error processing packet");
+            e.printStackTrace(new PrintStream(System.err));
         }
         
     }
     
     public void stop(){
+        System.out.println("Stopping server");
         try{
             srvr.close();
         }catch (Exception e){
